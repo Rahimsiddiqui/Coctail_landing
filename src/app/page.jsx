d8d6b6362265cae373a160698e6f53bf1906dffc
Rahim Siddiqui <rahimsiddiqui122@gmail.com>
2026-05-12 17:20:31 +0500

    Implement first part of hero secton.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 // Plugins
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
@@ -6,11 +8,14 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 
 // Components
 import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
 
 export default function LandingPage() {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="min-h-screen bg-black"></div>
     </main>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,59 @@
+import Image from "next/image";
+import { navLinks } from "../../constants";
+
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      },
+    );
+  }, []);
+
+  return (
+    <nav>
+      <div>
+        <a
+          href="#home"
+          className="flex items-center gap-2.5 transition-opacity hover:opacity-95"
+        >
+          <Image
+            src="/images/logo.png"
+            alt="Logo Image"
+            width={32}
+            height={32}
+            priority
+          />
+          <p>Velvet Pour</p>
+        </a>
+
+        <ul>
+          {navLinks.map((link, idx) => {
+            return (
+              <li key={idx + 1} className="transition-opacity hover:opacity-90">
+                <a href={link.id}>{link.title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
